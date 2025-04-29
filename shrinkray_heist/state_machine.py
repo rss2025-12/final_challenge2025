ca847ef4205e71c377767dbc07da09b2cdda4864
ruby@mit.edu <Ruby>
2025-04-28 20:06:40 -0400

    Initial state machine commit

---
+++ shrinkray_heist/state_machine.py
@@ -0,0 +1,17 @@
+import sys
+def main():
+    pass
+# def main():
+#     # sys.argv[0] is the name of the script
+#     print(f"Script name: {sys.argv[0]}")
+
+#     # The rest are the command line arguments
+#     if len(sys.argv) > 1:
+#         print("Arguments passed:")
+#         for i, arg in enumerate(sys.argv[1:], start=1):
+#             print(f"Argument {i}: {arg}")
+#     else:
+#         print("No arguments passed.")
+
+if __name__ == "__main__":
+    get_start_end()
